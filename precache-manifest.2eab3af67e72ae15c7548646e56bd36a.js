9db4fc078a3e431aaf7247da1af5665f95c0e3be
unknown <krithik2501@gmail.com>
2020-05-11 04:43:29 +0530

    Updates

---
+++ precache-manifest.2eab3af67e72ae15c7548646e56bd36a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38aea15d945adaeb355e79ec96af6b21",
+    "url": "/ukifunwork3bykriti/index.html"
+  },
+  {
+    "revision": "76252b488fca9ee6f84e",
+    "url": "/ukifunwork3bykriti/static/css/main.0f0555aa.chunk.css"
+  },
+  {
+    "revision": "c5fcff42bf7081fbff75",
+    "url": "/ukifunwork3bykriti/static/js/2.61e06272.chunk.js"
+  },
+  {
+    "revision": "76252b488fca9ee6f84e",
+    "url": "/ukifunwork3bykriti/static/js/main.64c4d21e.chunk.js"
+  },
+  {
+    "revision": "9f5752b046c2c4367bb1",
+    "url": "/ukifunwork3bykriti/static/js/runtime~main.782f9e4a.js"
+  },
+  {
+    "revision": "15b95755290cc2f4391b6ccef3f46308",
+    "url": "/ukifunwork3bykriti/static/media/38030.15b95755.jpg"
+  }
+]);
